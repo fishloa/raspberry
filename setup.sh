28c10e67f8b2aa115b26d9ae3c90b56d985570cf
Alex Fishlock <alex.fishlock@racingjag.com>
2020-11-29 00:27:44 +0000

    minor stuff

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root"
    exit 1
 fi
+echo "configuring time"
 timedatectl set-timezone Europe/London
 timedatectl set-ntp true
 cat <<EOT >> /etc/systemd/timesyncd.conf
@@ -14,12 +15,17 @@ PollIntervalMinSec=32
 PollIntervalMaxSec=2048
 EOT
 
+echo "updating packages"
 apt update
 apt -y upgrade
 apt install -y raspi-config rpi-eeprom
+
+echo "updating eeprom image"
 rpi-eeprom-update -d -a
 
 echo "installing raspi-config"
+if [ -e "/tmp/raspi-config_20200601_all.deb" ]; then rm /tmp/raspi-config_20200601_all.deb; fi
+
 wget https://archive.raspberrypi.org/debian/pool/main/r/raspi-config/raspi-config_20200601_all.deb -P /tmp
 apt -y install libnewt0.52 whiptail parted triggerhappy lua5.1 alsa-utils
 apt install -fy
