e55c92286fe9c612891c43161b4c53bf19d19e02
Alex Fishlock <alex.fishlock@racingjag.com>
2020-11-29 00:15:59 +0000

    added apt upgrading and eeprom updating

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
 timedatectl set-timezone Europe/London
 timedatectl set-ntp true
 cat <<EOT >> /etc/systemd/timesyncd.conf
@@ -9,3 +13,7 @@ RootDistanceMaxSec=5
 PollIntervalMinSec=32
 PollIntervalMaxSec=2048
 EOT
+apt install -y rpi-eeprom
+apt update
+apt -y upgrade
+rpi-eeprom-update -d -a
